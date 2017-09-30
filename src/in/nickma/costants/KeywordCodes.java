e804b15d69d5e720cc0a5d1d6aea8e3823e81008
Nick Main <nickmaster24@gmail.com>
2017-09-30 13:24:02 -0400

    Added constant class

---
+++ src/in/nickma/costants/KeywordCodes.java
@@ -0,0 +1,11 @@
+package in.nickma.costants;
+
+public class KeywordCodes {
+
+    public static final int SET_CODE = 1000;
+    public static final int DEFINE_CODE = 1001;
+
+    private KeywordCodes() {
+
+    }
+}
